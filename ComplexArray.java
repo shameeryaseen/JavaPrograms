9696a80cdaa301d6d7d50df09fbe7b225f563315
Mahammad shameer yaseen <79212725+shameeryaseen@users.noreply.github.com>
2024-11-08 00:28:49 +0530

    Add files via upload

---
+++ ComplexArray.java
@@ -0,0 +1,17 @@
+package classwork;
+
+public class ComplexArray {
+	class student{
+		int sid;
+		String sname;
+		int mobileNumber;
+	} student s1=new student();
+	
+	public  void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println(s1);
+			
+	}
+	
+
+}
